111ff6ee099509edfd6bf3b9f5055969c817fd2e
Joberoi23 <jobe0005@student.monash.edu>
2024-10-04 13:47:11 +1000

    adding my app

--- R/run-app.R
+++ R/run-app.R
@@ -3,3 +3,20 @@ run_app <- function() {
   app_dir <- system.file("eda-app", package = "cwdata")
   shiny::runApp(app_dir, display.mode = "normal")
 }
+
+#' @title title
+#' @description
+#' A short description...
+#' @returns nothing
+#' @examples
+#' \dontrun{
+#' run_my_app()
+#' }
+#'
+#'
+#' @export
+run_my_app <- function() {
+  app_dir <- system.file("my-app", package = "cwdata")
+  shiny::runApp(app_dir, display.mode = "normal")
+}
+
